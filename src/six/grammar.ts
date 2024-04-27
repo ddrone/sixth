312045fca4d42d04631ef5c035bc6a7cb95b4b00
Andrew Shulaev <ash.drone@gmail.com>
2024-04-27 17:02:56 +0100

    A beginning of an interpreter

---
+++ src/six/grammar.ts
@@ -0,0 +1,90 @@
+interface ConstExpr {
+  kind: 'const';
+  value: number;
+}
+
+interface CallExpr {
+  kind: 'call';
+  name: string;
+}
+
+type Expr = ConstExpr | CallExpr;
+
+function constExpr(n: number): Expr {
+  return {
+    kind: 'const',
+    value: n
+  }
+}
+
+function call(name: string): Expr {
+  return {
+    kind: 'call',
+    name
+  }
+}
+
+interface NumberValue {
+  kind: 'number';
+  value: number;
+}
+
+type Value = NumberValue;
+
+interface VmState {
+  dataStack: Value[];
+}
+
+class EvalError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+function popValue(state: VmState): Value {
+  const result = state.dataStack.pop();
+  if (result === undefined) {
+    throw new EvalError('Stack underflow!');
+  }
+  return result;
+}
+
+function pushValue(state: VmState, value: Value) {
+  state.dataStack.push(value);
+}
+
+function popNumber(state: VmState): number {
+  const result = popValue(state);
+  if (result.kind !== 'number') {
+    throw new EvalError(`Expected number, got ${result.kind}`)!
+  }
+  return result.value;
+}
+
+function pushNumber(state: VmState, value: number) {
+  pushValue(state, { kind: 'number', value });
+}
+
+const primitiveHandlers: Record<string, (state: VmState) => void> = {
+};
+
+type Program = Expr[];
+
+function evalProgram(program: Program, state: VmState) {
+  for (const expr of program) {
+    switch (expr.kind) {
+      case 'const': {
+        pushNumber(state, expr.value);
+        break;
+      }
+      case 'call': {
+        const handler = primitiveHandlers[expr.name];
+        if (handler === undefined) {
+          throw new EvalError(`Unknown function ${expr.name}`);
+        }
+        handler(state);
+        break;
+      }
+    }
+  }
+}
